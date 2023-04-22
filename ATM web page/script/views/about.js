a6961e256ff36a54bf14d4aba71250db67a26d17
AlexTS3 <tsvetanovalex@yahoo.com>
2023-04-22 15:46:32 +0300

    Added about page background

--- ATM web page/script/views/about.js
+++ ATM web page/script/views/about.js
@@ -2,11 +2,11 @@ import { html } from "../../node_modules/lit-html/lit-html.js";
 
 
 const aboutTemplate = () => html`
-<h3>This is the About Page</h3>`;
+<h3 class="about">This is the About Page</h3>`;
 
 
 export function aboutPage(ctx) {
     ctx.render(aboutTemplate());
-    document.body.style.backgroundImage = "url('../../resources/background.png')";
+    document.body.style.backgroundImage = "url('../../resources/about.png')";
 
 }
